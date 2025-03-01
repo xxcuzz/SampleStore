f6e5ac4ee8dd39a2215680bf4c95f6ea53373552
Vlad <dupanovvladislav@gmail.com>
2025-03-01 05:17:25 +0300

    Revert "BE: Add product and collection entities"
    This reverts commit 4c36e02dc52e554d837faf46ded1a9de49dc8888.

--- SampleStore.Domain/Entities/Collection.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SampleStore.Domain.Entities
-{
-    public class Collection
-    {
-        public required Guid Id { get; set; }
-        public required string Name { get; set; }
-    }
-}
